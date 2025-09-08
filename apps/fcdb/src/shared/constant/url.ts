62ea5e37dbb6503f5c2163b454810ff482b38f2b
jungbyungwook <dev2020196@gmail.com>
2025-09-08 16:21:02 +0000

    fix: baseUrl 못 가져옴 수정

--- apps/fcdb/src/shared/constant/url.ts
+++ apps/fcdb/src/shared/constant/url.ts
@@ -1,4 +1,6 @@
+import { isProd } from "@/shared/lib/environment";
+
 export const FCONLINE_API_URL = "https://open.api.nexon.com/fconline";
-export const baseUrl = process.env.isProd
+export const baseUrl = isProd
   ? "https://www.fcdb.co.kr"
   : "http://localhost:3000";
